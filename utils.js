8c153c6a365e9cc53a72b16fea95339806669d43
ingluves <127748109+ingluves@users.noreply.github.com>
2026-03-08 18:37:22 +0300

    Patch security vulnerability in authentication

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-08T15:37:17.877Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-08T15:37:23.510Z
+Commit: Patch security vulnerability in authentication
