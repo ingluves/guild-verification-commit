5678a463ed66fef55df644fabe9e812d0fd61404
ingluves <127748109+ingluves@users.noreply.github.com>
2026-03-08 18:38:31 +0300

    Fix CSS styling bug on mobile devices

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-08T15:38:18.766Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-08T15:38:32.038Z
+Commit: Fix CSS styling bug on mobile devices
