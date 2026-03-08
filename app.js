e5f3292cbe08a58f4e709c35454444a4cf61f40f
ingluves <127748109+ingluves@users.noreply.github.com>
2026-03-08 18:37:29 +0300

    Resolve null pointer exception in user service

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-08T15:37:27.606Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-08T15:37:30.678Z
+Commit: Resolve null pointer exception in user service
