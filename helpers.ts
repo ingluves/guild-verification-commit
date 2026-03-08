5f35150ee8e528442e7e8ce1ac42e420a678f802
ingluves <127748109+ingluves@users.noreply.github.com>
2026-03-08 18:38:24 +0300

    Add export functionality for reports

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-08T15:38:24.374Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-08T15:38:25.374Z
+Commit: Add export functionality for reports
