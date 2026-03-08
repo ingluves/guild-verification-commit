4b5405113cd4a8dd8f3623063473a6624ad08a31
ingluves <127748109+ingluves@users.noreply.github.com>
2026-03-08 18:37:25 +0300

    Patch API endpoint error handling

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-08T15:37:21.318Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-08T15:37:26.630Z
+Commit: Patch API endpoint error handling
