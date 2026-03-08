200f127b5a730de22b2540817f4976ea72dcd20a
ingluves <127748109+ingluves@users.noreply.github.com>
2026-03-08 18:37:16 +0300

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-08T15:37:01.494Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-08T15:37:17.877Z
+Commit: Fix CSS styling bug on mobile devices
