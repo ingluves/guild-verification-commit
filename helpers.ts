657f3e7ae8b9ab437e21a8eeb5c334285bba7af4
ingluves <127748109+ingluves@users.noreply.github.com>
2026-03-08 18:38:11 +0300

    Patch API endpoint error handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-08T15:37:58.351Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-08T15:38:12.014Z
+Commit: Patch API endpoint error handling
